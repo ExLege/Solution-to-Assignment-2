aee08043af79f8622781ab48ba9d5ac5c88a63d5
Grieves <rukevwe@hotmail.fr>
2021-04-08 16:18:32 -0400

    Add files via upload

---
+++ geo.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define M_PI 3.14159265358979323846
+
+double square_area(double length) {return pow(length, 2);}
+
+double volume_sphere(double radius1) {return (1.33333333333)*M_PI*radius1*radius1*radius1;}
+
+double cube_surface_area(double edge) {return 6 * pow(edge,2);}
+
+int main()
+{
+   
+   int ch;
+   double a,s,v,r,check;
+
+
+   do
+   {
+       
+       printf("1) Compute the area of a square");
+       printf("\n2) Compute the volume of a sphere");
+       printf("\n3) Compute the surface area of a cube\n4) Quit");
+       printf("\nEnter an option: ");
+       
+       scanf("%d",&ch);
+       switch(ch)
+       {
+           
+           case 1:
+                
+               printf("\nEnter the side length (cm): ");
+               scanf("%lf",&a);
+               
+               check = square_area(a);
+
+               printf("Area of the square: %.2f (cm)\n\n",check);
+               break;
+
+           
+           case 2:
+               printf("\nEnter the radius (cm): ");
+               
+               scanf("%lf",&r);
+               
+               check = volume_sphere(r);
+
+               printf("Volume of the Sphere is %.2f (cm)\n\n",check);
+
+               break;
+
+           case 3:
+               
+               printf("\nEnter the edge of the cube: ");
+
+               scanf("%lf",&s);
+               
+               check = cube_surface_area(s);
+
+               printf("Surface of the cube %.2f (cm)\n\n",check);
+
+               break;
+           case 4:
+               
+               printf("\nGoodbye!");
+
+               exit(0);
+           
+           default:
+               printf("Choose valid selection\n");
+       }
+   }
+   while (ch!=4);       
+}
+
---
+++ invest.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+int main()
+{
+
+	
+	double invest,rate,add, start,end,interest;
+	int year;
+	
+	
+	do
+	{
+        
+		printf("Enter initial investment amount: ");
+	
+	    scanf("%lf",&invest);
+        
+        if (invest<0)
+        {
+            printf("Invalid data entered. Initial investment cannot be negative.\n");
+		    
+        }
+	}while(invest<0);
+	
+	do
+	{
+		
+        printf("Enter total years: ");
+        
+        scanf("%d",&year);
+
+        if (year<=0)
+        {
+            printf("Invalid data entered. Years must be greater than 0.\n");
+		    
+        }
+
+	}while(year<=0);
+
+	
+
+	
+	do
+	{
+		
+        printf("Enter return rate: ");
+        
+        scanf("%lf",&rate);
+
+        if (rate<0)
+        {
+            printf("Invalid data entered. Rate cannot be negative.\n");
+		    
+        }
+
+	}while(rate<0);
+
+	
+
+	
+	do
+	{   
+        printf("Enter additional contribution per year: ");
+            
+        scanf("%lf",&add);
+
+        if (add<0)
+        {
+            printf("Invalid data entered. Contribution cannot be negative.\n");
+		    
+        }
+        
+
+	}while(add<0);
+	
+
+	printf("\n");
+	printf("%-10s","Year");
+
+	printf("%-20s","Start Balance");
+
+	printf("%-20s","Interest");
+
+	printf("%-20s","End Balance");
+
+	printf("\n**************************************************************************\n");
+
+	start = invest;
+
+	for(int i=1;i<=year;i++)
+	{
+
+
+		
+		interest=start*rate/100;
+		
+		end=start+interest;
+		
+		printf("%-10d",i);
+		
+		
+
+		printf("%-20.2f",start);
+
+		printf("%-20.2f",interest);
+		
+		printf("%-20.2f",end);
+		
+		printf("\n");
+		
+		start=end+add;
+
+}
+
+return 0;
+
+}
